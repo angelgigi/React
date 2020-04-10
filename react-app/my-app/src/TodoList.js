272b52787495a722e8b3ff2cd35090895b77b93a
anglegigi <angelqiqiwang@gmail.com>
2020-04-10 16:56:15 +0800

    父子组件传值

--- react-app/my-app/src/TodoList.js
+++ react-app/my-app/src/TodoList.js
@@ -1,55 +1,67 @@
 /**
  * Created by angelqiqi on 2020/4/8.
  */
-import React, { Component , Fragment} from 'react';
-import './style.css'
+import React, {Component, Fragment} from 'react';
+import './style.css';
+import TodoItem from './TodoItem'
 
 class TodoList extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      inputValue:'',
-      list:['学习数学','学习react']
+      inputValue: '',
+      list: ['学习数学', '学习react']
     }
   }
-  render(){
-    return(
+
+  render() {
+    return (
       <Fragment>
         {/*大写开头是组件*/}
         <div>
           <label htmlFor="inserArea">输入内容</label>
-          <input className="input" value={this.state.inputValue} onChange={this.handleInputClick.bind(this)} />
+          <input className="input" value={this.state.inputValue} onChange={this.handleInputClick.bind(this)}/>
           <button onClick={this.handleBtnClick.bind(this)}>提交</button>
         </div>
         <ul>
-          {this.state.list.map((item,index) =>{
-            return <li key={index}
+          {this.state.list.map((item, index) => {
+            return (
+              <div>
+                {/*父传子：子组件通过this.props接收*/}
+                <TodoItem content={item} index={index} deleteItem={this.handleItemDelete.bind(this)} />
+                
+                {/*<li key={index}
                        onClick={this.handleItemDelete.bind(this,index)}
                        dangerouslySetInnerHTML = {{__html:item}}
-            ></li>
+                 ></li>*/}
+              </div>
+            )
           })}
         </ul>
       </Fragment>
     )
   }
-  handleInputClick(e){
+
+  handleInputClick(e) {
     this.setState({
-      inputValue:e.target.value
+      inputValue: e.target.value
     })
   }
-  handleBtnClick(){
+
+  handleBtnClick() {
     this.setState({
-      list:[...this.state.list,this.state.inputValue],
-      inputValue:''
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
     })
   }
-  handleItemDelete(index){
+
+  handleItemDelete(index) {
     const list = [...this.state.list];
-    list.splice(index,1);
+    list.splice(index, 1);
 
     this.setState({
-      list:list
+      list: list
     })
   }
 }
